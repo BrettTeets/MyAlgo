fac1350212a6331daf6de94ee06d6406f0898a54
Brett <opsfox245@gmail.com>
2024-01-12 11:32:12 -0600

    First

---
+++ BAlgo/source/QuickSort.cs
@@ -0,0 +1,32 @@
+﻿namespace BAlgo;
+
+public static class QuickSort<T> where T: IComparable
+{
+    private static int Partition(T[] array, int left, int right) {
+        IComparable pivot = array[right];
+
+        int swapIndex = left - 1;
+
+        for(int currentIndex = left; currentIndex < right; currentIndex++){
+            if(array[currentIndex].CompareTo(pivot) <= 0){
+                swapIndex += 1;
+
+                //Using tuples to do the swap. from c#7
+                (array[currentIndex], array[swapIndex]) = (array[swapIndex], array[currentIndex]);
+            }
+        }
+
+        swapIndex += 1;
+        (array[right], array[swapIndex]) = (array[swapIndex], array[right]);
+        return swapIndex;
+    }
+
+    public static void Sort(T[] array, int left, int right){
+        if(left >= right || left < 0) return;
+
+        int pivotIndex = Partition(array, left, right);
+
+        Sort(array, left, pivotIndex-1);
+        Sort(array, pivotIndex+1, right);
+    }
+}
---
+++ MyConsole/Program.cs
@@ -0,0 +1,24 @@
+﻿using BAlgo;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        Console.WriteLine("Before: ");
+        double[] array = [1.6, 1.9, 2.3, 8.4, 0.1, 3.2, 7.1, 6.4, 3.3, 9.9, 4.8, 4.2, 6.9, 3.1, 1.2, 7.7, 3.9];
+
+        DebugArray(array);
+        Console.WriteLine("After: ");
+
+        QuickSort<double>.Sort(array, 0, array.Length-1);
+
+        DebugArray(array);
+    }
+
+    private static void DebugArray<T>(T[] array){
+        foreach(T o in array){
+            if(o is not null) Console.Write(o.ToString() + " ");
+        }
+    }
+}
